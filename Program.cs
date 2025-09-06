14a2a8c8ba9ca01516753fe2cb2398932fcaff36
srtfdsfsdfsdf <asdasd@sadsadas.asdasd>
2025-09-06 11:03:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace ISP {
+    class Program {
+        static void Main() {
+            //Console.WriteLine("Здарова!!!");
+            //int num = 10;
+            //Console.WriteLine(num);
+            //Console.WriteLine("Введите ваше имя:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя - {name}");
+
+
+
+            //Console.WriteLine("Возраст");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Ваше лет - {age}");
+
+            bool a = true;
+            bool b = false;
+            char sym = '2';
+            double c = 3.4;
+            decimal f = 3.4M;
+            var q = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
